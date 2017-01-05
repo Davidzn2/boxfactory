587f8e88f232552ddc19563a01af2cea7f350f1f
davidzn2 <davidzn2@gmail.com>
2017-01-05 13:48:09 -0600

    Agregue pagina como trabajamos y cuadrosga .

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
-      	sectionsColor : ['white','#3cc8b5'],
+      	sectionsColor : ['white','#3cc8b5','#3cc8b5','#042c5b'],
+
     });
 });
