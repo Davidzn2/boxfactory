44f6907e057990f897789cc5e6139565719bcf73
davidzn2 <davidzn2@gmail.com>
2017-01-06 11:34:54 -0600

    V1.5

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
+        anchors: ['firstPage', 'Nosotros', 'Nosotros','Como-Trabajamos','Contactanos'],
       	sectionsColor : ['white','#3cc8b5','#3cc8b5','#042c5b','#e6b900'],
+        navigation: true,
+        css3: true
 
     });
 });
