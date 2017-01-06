650d2d27fbc6d806be55b4620fc03990875f1813
davidzn2 <davidzn2@gmail.com>
2017-01-06 14:12:55 -0600

    V 2.0

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
-        anchors: ['firstPage', 'Nosotros', 'Nosotros','Como-Trabajamos','Contactanos'],
-      	sectionsColor : ['white','#3cc8b5','#3cc8b5','#042c5b','#e6b900'],
+        anchors: ['firstPage', 'Nosotros', 'Areas-de-trabajo','Como-Trabajamos','Contactanos'],
+      	sectionsColor : ['white','white','#3cc8b5','#042c5b','#e6b900'],
         navigation: true,
         css3: true
 
