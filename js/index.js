37ec74cbde3cd99b7748eaef787cf7611d1967da
davidzn2 <davidzn2@gmail.com>
2017-01-09 09:49:08 -0600

    Arreglos

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
-        anchors: ['firstPage', 'Nosotros', 'Areas-de-trabajo','Como-Trabajamos','Contactanos'],
+        anchors: ['', 'Nosotros', 'Areas-de-trabajo','Como-Trabajamos','Contactanos'],
       	sectionsColor : ['white','white','#3cc8b5','#042c5b','#e6b900'],
         navigation: true,
         css3: true
