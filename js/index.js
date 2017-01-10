9feb61a0b1b8b92820ab5f66e4236f055ed875ae
davidzn2 <davidzn2@gmail.com>
2017-01-10 11:45:06 -0600

    Arreglos

--- js/index.js
+++ js/index.js
@@ -6,4 +6,19 @@ $(document).ready(function() {
         css3: true
 
     });
+    $('.materialboxed').materialbox();
+    $('.modal').modal({
+            dismissible: true, // Modal can be dismissed by clicking outside of the modal
+            opacity: .5, // Opacity of modal background
+            in_duration: 300, // Transition in duration
+            out_duration: 200, // Transition out duration
+            starting_top: '4%', // Starting top style attribute
+            ending_top: '10%', // Ending top style attribute
+
+          }
+        );
+
+    $('.carousel').carousel();
+
+
 });
