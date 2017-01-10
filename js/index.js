4d6f0fdd49770a8de42d6e23b0ebb6f4a2867d74
davidzn2 <davidzn2@gmail.com>
2017-01-10 16:36:40 -0600

    Agregue galerias

--- js/index.js
+++ js/index.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
         anchors: ['', 'Nosotros', 'Areas-de-trabajo','Como-Trabajamos','Contactanos'],
       	sectionsColor : ['white','white','#3cc8b5','#042c5b','#e6b900'],
         navigation: true,
-        css3: true
+        css3: true,
+        menu:'#menu'
 
     });
     $('.materialboxed').materialbox();
@@ -19,6 +20,17 @@ $(document).ready(function() {
         );
 
     $('.carousel').carousel();
+        $('.dropdown-button').dropdown({
+        inDuration: 300,
+        outDuration: 225,
+        constrain_width: false, // Does not change width of dropdown to that of the activator
+        hover: true, // Activate on hover
+        gutter: 0, // Spacing from edge
+        belowOrigin: false, // Displays dropdown below the button
+        alignment: 'left' // Displays dropdown with edge aligned to the left of button
+      }
+);
+      
 
 
 });
---
+++ js/index2.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        anchors: [ 'gastro', 'personal','retail','branding'],
+      	sectionsColor : ['#e6b900','#252525','#e84a92','#3cc8b5',],
+        navigation: true,
+        css3: true,
+        menu:'#menu'
+
+    });
+
+
+});
