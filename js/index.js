c2832664574516d424ba0e7f39bed3575ddad4f0
davidzn2 <davidzn2@gmail.com>
2017-01-10 18:36:40 -0600

    Agregue imagenes a galeria

--- js/index.js
+++ js/index.js
@@ -8,19 +8,10 @@ $(document).ready(function() {
 
     });
     $('.materialboxed').materialbox();
-    $('.modal').modal({
-            dismissible: true, // Modal can be dismissed by clicking outside of the modal
-            opacity: .5, // Opacity of modal background
-            in_duration: 300, // Transition in duration
-            out_duration: 200, // Transition out duration
-            starting_top: '4%', // Starting top style attribute
-            ending_top: '10%', // Ending top style attribute
 
-          }
-        );
 
     $('.carousel').carousel();
-        $('.dropdown-button').dropdown({
+    $('.dropdown-button').dropdown({
         inDuration: 300,
         outDuration: 225,
         constrain_width: false, // Does not change width of dropdown to that of the activator
@@ -28,9 +19,8 @@ $(document).ready(function() {
         gutter: 0, // Spacing from edge
         belowOrigin: false, // Displays dropdown below the button
         alignment: 'left' // Displays dropdown with edge aligned to the left of button
-      }
-);
-      
+      });
+
 
 
 });
