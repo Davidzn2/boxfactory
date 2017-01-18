4fa3b6fd542002680684e92121ee0f7eb2157a69
davidzn2 <davidzn2@gmail.com>
2017-01-17 19:43:20 -0600

    Arreglos en colores

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
         anchors: ['', 'Nosotros', 'Areas-de-trabajo','Como-Trabajamos','Contactanos'],
-      	sectionsColor : ['white','white','#3cc8b5','#042c5b','#e6b900'],
+      	sectionsColor : ['white','white','#3cc8b5','#919191','#383838'],
         navigation: true,
         css3: true,
         menu:'#menu'
